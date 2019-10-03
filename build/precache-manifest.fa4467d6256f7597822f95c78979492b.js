bfdf4773a946d8c986170873d2564db1cdff858d
DavidCAriasR <dacamarro@hotmail.com>
2019-10-03 18:17:33 -0500

    very
    I'm happy

---
+++ build/precache-manifest.fa4467d6256f7597822f95c78979492b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9ad849904b40c63a13eaa7a3bc837b",
+    "url": "anjonnes.github.io/anjobel/index.html"
+  },
+  {
+    "revision": "87374f9f8247b984eb8a",
+    "url": "anjonnes.github.io/anjobel/static/css/main.d8cf9644.chunk.css"
+  },
+  {
+    "revision": "ac5fc3f87b58a884301e",
+    "url": "anjonnes.github.io/anjobel/static/js/2.f1383dde.chunk.js"
+  },
+  {
+    "revision": "87374f9f8247b984eb8a",
+    "url": "anjonnes.github.io/anjobel/static/js/main.86df5f20.chunk.js"
+  },
+  {
+    "revision": "5997c2ceb22f93f49f93",
+    "url": "anjonnes.github.io/anjobel/static/js/runtime-main.4ef4f271.js"
+  }
+]);
---
+++ src/components/saludar/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import'./estilo.css';
+ class Saludar extends Component{
+   render(){
+     return(
+       <h1>(: Hello :)</h1>
+     );
+   }
+ }
+ export default Saludar;
